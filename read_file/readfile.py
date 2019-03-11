bfc793dcf71b9fa17008a0f96d6543c169fad30f
chLei <993716335@qq.com>
2019-03-11 14:14:16 +0800

    fix

--- read_file/readfile.py
+++ read_file/readfile.py
@@ -1,12 +1,12 @@
 # coding:utf-8
 import commands
 
-with open('poem.txt', 'r') as f:
+with open('read_file/poem.txt', 'r') as f:
     result = list()
     for line in f.readlines():
         line = line.strip()
         a, b = commands.getstatusoutput('ls ' + line)
         result.append(line)
-        result.append(a)
+        result.append(str(a))
         result.append(b)
-open('result-readline.txt', 'w').write('%s' % '\n'.join(result))
+open('read_file/result-readline.txt', 'w').write('%s' % str('\n'.join(result)))
